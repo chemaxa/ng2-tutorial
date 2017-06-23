70c5e6b27671eb0f42cc4b43a20ccb54879b7cb8
Maksim <maksim.shkarbaliuk@sotsos.com>
2017-06-23 10:59:28 +0300

    Init

---
+++ src/child.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+    selector: 'child-comp',
+    template: `<button (click)="change(true)">+</button>
+               <button (click)="change(false)">-</button>`
+})
+export class ChildComponent {
+
+    @Output() onChanged = new EventEmitter<boolean>();
+    change(increased: any) {
+
+        this.onChanged.emit(increased);
+    }
+}
