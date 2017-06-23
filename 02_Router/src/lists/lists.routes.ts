e8036545d6ef9ce2babdadb079019d169a068032
Maksim <maksim.shkarbaliuk@sotsos.com>
2017-06-23 14:54:53 +0300

    Lesson 2 Added samples with inputs, outputs, props

--- 02_Router/src/lists/lists.routes.ts
+++ 02_Router/src/lists/lists.routes.ts
@@ -2,5 +2,6 @@ import { List1Component, List2Component } from './index'
 export const routes = [
     { path: 'list1', component: List1Component },
     { path: 'list2', component: List2Component },
-    { path: '', redirectTo: 'list1', pathMatch: 'full' }
+    { path: '', redirectTo: 'list1', pathMatch: 'full' },
+    { path: "02_Router", redirectTo: "list1", pathMatch: "full" },
 ]
